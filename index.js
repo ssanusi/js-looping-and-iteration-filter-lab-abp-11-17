830531d9c8b214a9b2f39d13c013cdc20fb7bfb6
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-11 11:23:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers,string) {
   return drivers.filter(function (e) {
-    return e === string || e.toLowerCase() == string
+    return e === string || e.toLowerCase() === string.toLowerCase()
 
   })
 
