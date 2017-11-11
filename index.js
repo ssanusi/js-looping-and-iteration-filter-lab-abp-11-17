6a44bb910fdcabc165b9f2fd24fe6b99e3b720fb
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-11 10:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers,string) {
+  return drivers.filter(function (e) {
+    return e === string || e == string
+
+  })
+
+}
+function matchName(drivers,string) {
+  return drivers.filter(function (e) {
+    return e.name === string 
+
+  })
+
+}
