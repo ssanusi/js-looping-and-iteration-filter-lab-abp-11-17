63769bbd8f2db6b1fd306ad70857d6c35a919692
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-11 11:23:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(drivers,string) {
+  return drivers.filter(function (e) {
+    return e === string || e.toLowerCase() === string.toLowerCase()
+
+  })
+
+}
+function fuzzyMatch(drivers,string) {
+  return drivers.filter(function(e){
+    return e[0] === string[0];
+  })
+
+}
+function matchName(drivers,string) {
+  return drivers.filter(function (e) {
+    return e.name === string;
+
+  })
+
+}
